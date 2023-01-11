4874a557103c9bb93de49fa9a2a07f0473a329e7
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-11 17:01:07 +0100

    Add MILESTONE-2 & 3

--- js/vue.js
+++ js/vue.js
@@ -1,5 +1,15 @@
-console.log('Vue ok', Vue);
-
+/*
+MILESTONE 1
+Stampare all'interno di una lista HTML un item per ogni task.
+Se la proprietà done è uguale a true, visualizzare il testo del task sbarrato.
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il task viene rimosso dalla lista.
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei todo esistenti.
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)console.log('Vue ok', Vue);
+*/
 const app = Vue.createApp({
     data() {
         return {
@@ -8,12 +18,26 @@ const app = Vue.createApp({
                     {text: 'dormire', done: false} ],
                 }
     },
-    computed: {       
+    computed: {    
+        newTask() {
+           return newTask = {text: '', done: true}
+        }
+
     },
     methods: {
         deletTask(i) {
             this.tasks.splice( i, 1 )
         },
+        addTask() {
+            if (this.newTask.text) {
+                this.tasks.push(this.newTask);
+                // this.clearInput()
+            }
+        },
+
+        clearInput(){
+                this.newTask.text = '';
+        }
     
     }
 
