e6c48ff5477cb8334fce2f9bbd5ae98cf1d82fd3
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-11 15:35:54 +0100

    Add Mileston 1

--- js/vue.js
+++ js/vue.js
@@ -1,1 +1,23 @@
 console.log('Vue ok', Vue);
+
+const app = Vue.createApp({
+    data() {
+        return {
+            tasks: [{text: 'fare la spesa', done: true},
+                    {text: 'mangiare', done: false},
+                    {text: 'dormire', done: false} ],
+                }
+    },
+    computed: {       
+    },
+    methods: {
+        deletTask(i) {
+            this.tasks.splice( i, 1 )
+        },
+    
+    }
+
+})
+
+app.mount('#root');
+
