9f9fade148f6b50614edb7ff67df19473ee377b1
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-12 17:44:32 +0100

    Add bonus-2

--- js/vue.js
+++ js/vue.js
@@ -8,8 +8,10 @@ MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo task, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il task alla lista
-2- cliccando sul testo dell'item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)console.log('Vue ok', Vue);
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del task corrispondente (se done era uguale a false, impostare true e viceversa)
 */
+console.log('Vue ok', Vue);
+
 const app = Vue.createApp({
     data() {
         return {
@@ -21,26 +23,27 @@ const app = Vue.createApp({
     computed: {    
         newTask() {
            return newTask = {text: '', done: false}
-        }
-
+        },
     },
     methods: {
         deletTask(i) {
-            this.tasks.splice( i, 1 )
+            this.tasks.splice(i, 1)
         },
         addTask() {
             if (this.newTask.text) {
-                this.tasks.push(this.newTask);
-                // this.clearInput()
+                const task = {...this.newTask};
+                this.tasks.push(task);
+                this.clearInput()
             }
         },
-
         clearInput(){
                 this.newTask.text = '';
+        },
+        changeDone(i){
+            this.tasks[i].done = !this.tasks[i].done;
+            
         }
-    
     }
-
 })
 
 app.mount('#root');
